238ac1a8f485bf22d338ad780d5c7547330ad326
Your Name <robertcjohnson1984@gmail.com>
2021-08-14 14:27:15 -0600

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const routes = require('./routes');
 const mongoose = require ("mongoose");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 const app = express();
 
 require('dotenv').config();
