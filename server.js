690a97d22dfaa8d9b74a48eac3929e0f72fdef29
Your Name <robertcjohnson1984@gmail.com>
2021-08-13 19:13:08 -0600

    add mongo connect and routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const path = require("path");
+const routes = require('./routes');
+const mongoose = require ("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,6 +20,13 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Routes
+app.use(routes);
+
+// Connects to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+
+// Starts API server
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
