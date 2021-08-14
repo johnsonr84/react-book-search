07213c588ab9debba9d5590f5d0b9553c86a3dad
Your Name <robertcjohnson1984@gmail.com>
2021-08-13 19:50:21 -0600

    add routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path={["/", "/books"]}>
+          <Books />
+        </Route>
+        <Route exact path="/books/:id">
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  );
 }
 
 export default App;
