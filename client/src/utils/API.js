41d57c2ee59f270517fa0587d3833d7fb7099fc4
Your Name <robertcjohnson1984@gmail.com>
2021-08-14 19:38:03 -0600

    update API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,27 +4,25 @@ export default {
   // Gets all books
   getBooks: function() {
     console.log("getbooks called");
-    return axios.get("/api/books");
+    return axios.get("/api/books/");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
+  
   // Deletes the book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
+
   // Saves a book to the database
   saveBook: function(bookData) {
     console.log("book data"+bookData.title);
-    
-    return( axios.post("api/books/", {
-      title: bookData.title,
-      image: bookData.image,
-      authors: bookData.authors,
-      description: bookData.description,
-      link: bookData.link,
-      volId: bookData.volID
-    }))
-  }
-};
+
+      return( axios.post("api/books/", {
+        title: bookData.title,
+        image: bookData.image,
+        authors: bookData.authors,
+        description: bookData.description,
+        link: bookData.link,
+        volId: bookData.volID
+      }))
+    }
+}
