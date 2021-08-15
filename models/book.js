ce9e99582c1c62ee36701347f311542b99b89c05
Your Name <robertcjohnson1984@gmail.com>
2021-08-14 19:29:19 -0600

    update bookSchema

--- models/book.js
+++ models/book.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  authors: { },
+  description: String,
+  image: { type: String},
+  link: {type: String},
+  volID: {type:String}
 });
 
 const Book = mongoose.model("Book", bookSchema);
